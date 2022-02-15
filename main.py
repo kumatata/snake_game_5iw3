7cbc127384b8efb2e4dfd5b6d773474e420e9bcb
Emeline GARO <emeline.garo@supinternet.fr>
2022-02-15 22:13:01 +0100

    add speed and slow functionality in game

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+medium_blue = (0, 0, 255)
 
 dis_width = 600
 dis_height = 400
+game_width = 600
+game_height = 350
 
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Jeu du python | 5IW3')
@@ -22,18 +25,17 @@ pygame.display.set_caption('Jeu du python | 5IW3')
 clock = pygame.time.Clock()
 
 snake_block = 10
-snake_speed = 8
 
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
 
-def Your_score(score):
+def your_score(score):
     value = score_font.render("Score: " + str(score), True, yellow)
-    dis.blit(value, [0, 0])
+    dis.blit(value, [0, game_height])
 
 
-   
+
 def show_last_score():
     if(os.path.isfile("score_file.txt") == True):
         all_score = []
@@ -45,13 +47,12 @@ def show_last_score():
 
             output = score_font.render("Last score: " +   last_score , True, green)
 
-            dis.blit(output, [0, 350])
-            # pygame.display.update()
+            dis.blit(output, [game_width/2, game_height])
 
 
-    
 
- 
+
+
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
@@ -62,7 +63,7 @@ def message(msg, color):
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
 
-def gameLoop():
+def game_loop():
     game_over = False
     game_close = False
 
@@ -73,17 +74,23 @@ def gameLoop():
     y1_change = 0
 
     snake_List = []
-    Length_of_snake = 1
+    length_of_snake = 1
+    snake_speed = 8
 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-    
+    food_x = round(random.randrange(0, game_width - snake_block) / 10.0) * 10.0
+    food_y = round(random.randrange(0, game_height - snake_block) / 10.0) * 10.0
+    speed_x = round(random.randrange(0, game_width - snake_block) / 10.0) * 10.0
+    speed_y = round(random.randrange(0, game_height - snake_block) / 10.0) * 10.0
+    slow_x = round(random.randrange(0, game_width - snake_block) / 10.0) * 10.0
+    slow_y = round(random.randrange(0, game_height - snake_block) / 10.0) * 10.0
+
+
     while not game_over:
         while game_close == True:
             dis.fill(blue)
             message(
                 "Vous avez perdu! Faites 'c' pour recommencer ou 'q' pour quitter ", red)
-            Your_score(Length_of_snake - 1)
+            your_score(length_of_snake - 1)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -92,7 +99,7 @@ def gameLoop():
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        gameLoop()
+                        game_loop()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -111,17 +118,19 @@ def gameLoop():
                     y1_change = snake_block
                     x1_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        if x1 >= game_width or x1 < 0 or y1 >= game_height or y1 < 0:
             game_close = True
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        pygame.draw.rect(dis, green, [food_x, food_y, snake_block, snake_block])
+        pygame.draw.rect(dis, red, [speed_x, speed_y, snake_block, snake_block])
+        pygame.draw.rect(dis, medium_blue, [slow_x, slow_y, snake_block, snake_block])
         snake_Head = []
         snake_Head.append(x1)
         snake_Head.append(y1)
         snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
+        if len(snake_List) > length_of_snake:
             del snake_List[0]
 
         for x in snake_List[:-1]:
@@ -130,28 +139,40 @@ def gameLoop():
 
         our_snake(snake_block, snake_List)
 
-        Your_score(Length_of_snake - 1)
+        your_score(length_of_snake - 1)
         show_last_score()
 
         pygame.display.update()
 
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(
-                0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(
-                0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
+        if x1 == food_x and y1 == food_y:
+            food_x = round(random.randrange(
+                0, game_width - snake_block) / 10.0) * 10.0
+            food_y = round(random.randrange(
+                0, game_height - snake_block) / 10.0) * 10.0
+            length_of_snake += 1
+        if x1 == speed_x and y1 == speed_y:
+            speed_x = round(random.randrange(
+                0, game_width - snake_block) / 10.0) * 10.0
+            speed_y = round(random.randrange(
+                0, game_height - snake_block) / 10.0) * 10.0
+            snake_speed += 1
+        if x1 == slow_x and y1 == slow_y:
+            slow_x = round(random.randrange(
+                0, game_width - snake_block) / 10.0) * 10.0
+            slow_y = round(random.randrange(
+                0, game_height - snake_block) / 10.0) * 10.0
+            snake_speed -= 1
 
         clock.tick(snake_speed)
 
     with open("score_file.txt", "a", newline='') as file_score:
-        score = str(Length_of_snake - 1) 
+        score = str(length_of_snake - 1)
         file_score.write(score + os.linesep)
     show_last_score()
     pygame.quit()
-    
+
 
     quit()
 
 
-gameLoop()
+game_loop()
--- readme.md
+++ readme.md
@@ -1,12 +1,18 @@
-# les fonctions de base utilisées 
-    - init() : initialises tous les modules importés de pygame 
+# Lancer le projet
+
+```
+pip install -r requirements.txt
+python3 main.py
+```
+
+# les fonctions de base utilisées
+
+    - init() : initialises tous les modules importés de pygame
     - display.set_mode(): récupère un tuple ou une liste en tant que propre paramètre pour créer une surface
     - update(): met à jour l'écran
     - quit(): désinitialise l'écran
     - set_caption(): s'occupe de modifier le texte du haut de l'écran
     - event.get(): retourne la liste des évènements
     - Surface.fill(): remplit la surface avec une couleur (solid)
-    - time.Clock(): le temps 
+    - time.Clock(): le temps
     - font.SysFont(): crée la font du pygame avec la font du système.
-
-
